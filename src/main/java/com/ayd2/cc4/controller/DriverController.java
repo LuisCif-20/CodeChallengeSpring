c326c4dd26f64f6ba087ef8c55df5a6d52cded1c
LuisCif-20 <n.rodas225@gmail.com>
2025-03-06 17:58:05 -0600

    controller updated

--- src/main/java/com/ayd2/cc4/controller/DriverController.java
+++ src/main/java/com/ayd2/cc4/controller/DriverController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ import com.ayd2.cc4.dto.DriverDto;
 import com.ayd2.cc4.model.Driver;
 import com.ayd2.cc4.service.DriverService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -27,7 +29,7 @@ public class DriverController {
     }
 
     @PostMapping()
-    public ResponseEntity<Driver> createDriver(DriverDto driverDto) {
+    public ResponseEntity<Driver> createDriver(@RequestBody @Valid DriverDto driverDto) {
         return ResponseEntity.status(201).body(this.driverService.createDriver(driverDto));
     }
     
